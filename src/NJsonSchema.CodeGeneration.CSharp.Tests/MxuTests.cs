9db3232f22d4c2007f6b973c61dc0ffbdbe97091
Martin Xu <martin.xu@ia.ca>
2020-09-28 14:30:41 -0700

    used updated json schema

--- src/NJsonSchema.CodeGeneration.CSharp.Tests/MxuTests.cs
+++ src/NJsonSchema.CodeGeneration.CSharp.Tests/MxuTests.cs
@@ -19,8 +19,8 @@ namespace NJsonSchema.CodeGeneration.Tests.CSharp
             @"{
   ""$schema"": ""http://json-schema.org/draft-07/schema#"",
   ""$id"": ""http://ia.com/payment.schema.json"",
-  ""title"": ""Bank Result Payment CMF"",
-  ""description"": ""Bank Result Payment CMF"",
+  ""title"": ""Dealer Service Payment CMF"",
+  ""description"": ""Dealer Service Payment CMF"",
   ""type"": ""object"",
   ""properties"": {
     ""id"": {
@@ -39,8 +39,8 @@ namespace NJsonSchema.CodeGeneration.Tests.CSharp
       ""description"": ""Unique ID of the message type. Use the functional name of the message (event or command), prefixed with the LOB code"",
       ""type"": ""string"",
       ""enum"": [
-        ""SAL.PaymentSucceeded"",
-        ""SAL.PaymentFailed""
+        ""DS.PaymentApproved"",
+        ""DS.PaymentCancelled""
       ]
     },
     ""version"": {
@@ -64,9 +64,9 @@ namespace NJsonSchema.CodeGeneration.Tests.CSharp
       ""description"": ""Indicates where the event is from"",
       ""type"": ""string"",
       ""enum"": [
-        ""SAL/UNIFI/EFT"",
-        ""SAL/UN/EFT"",
-        ""SAL/CAN/EFT""
+        ""DS/UNIFI/EFT"",
+        ""DS/UN/EFT"",
+        ""DS/CAN/EFT""
       ]
     },    
     ""datacontenttype"": {
@@ -88,113 +88,162 @@ namespace NJsonSchema.CodeGeneration.Tests.CSharp
       ""maxLength"": 30
     },
     ""env"": {
-      ""description"": ""Environment (ASMB, FNCT, INTG, ACCP, PROD, etc.)"",
+      ""description"": ""Environment (ASMB, FNCT, INTG, ACCP, PROD)"",
       ""type"": ""string"",
       ""minLength"": 4,
       ""maxLength"": 4
     },    
     ""data"": {
       ""type"": ""object"",
-      ""properties"": {
+	  ""properties"": {
         ""context"": {
           ""description"": ""Context"",
           ""type"": ""string"",
           ""minLength"": 1,
           ""maxLength"": 30
         },
-        ""bankReportDate"": {
-          ""description"": ""Must be formatted by using the standard RFC3339"",
-          ""type"": ""string"",
-          ""format"": ""date""
-        },
-        ""bankReportName"": {
-          ""description"": ""Name of Bank Report - EBR2131A, EBR2132A, EBR2133A, EBR2134A, EBR2135A"",
-          ""type"": ""string"",
-          ""minLength"": 1,
-          ""maxLength"": 20
-        },
-        ""paymentMethod"": {
-          ""description"": ""Payment Method - Electronic Funds Transfer"",
-          ""type"": ""string"",
-          ""enum"": [
-            ""EFT""
-          ]
-        },
-        ""languageCode"": {
-          ""description"": ""Language"",
-          ""type"": ""string"",
-          ""minLength"": 1,
-          ""maxLength"": 1
-        },
-        ""bankPaymentSuccessful"": {
-          ""description"": ""Bank Transaction Status - was payment successful or failed?"",
-          ""type"": ""boolean""
-        },
-        ""currency"": {
-          ""description"": ""Currency"",
-          ""type"": ""string"",
-          ""enum"": [
-            ""CAD"",
-            ""USD""
-          ]
-        },
-        ""companyCode"": {
-          ""description"": ""Company Code"",
-          ""type"": ""string"",
-          ""minLength"": 1,
-          ""maxLength"": 1
-        },
-        ""customerAccount"": {
-          ""description"": ""Customer Account"",
-          ""type"": ""string"",
-          ""minLength"": 10,
+		""cmfVersion"": {
+		  ""description"": ""CMF Version"",
+		  ""type"": ""string"",
+		  ""minLength"": 1,
+		  ""maxLength"": 5
+		},
+	    ""paymentId"": {
+		  ""description"": ""Payment Transaction Id"",
+		  ""type"": ""string"",
+		  ""minLength"": 1,
           ""maxLength"": 10
         },
-        ""authorizationNumber"": {
-          ""description"": ""Authorization Number"",
-          ""type"": ""string"",
-          ""minLength"": 1,
-          ""maxLength"": 18
-        },
-        ""contractDealerNumber"": {
-          ""description"": ""Contract Dealer Number"",
-          ""type"": ""string"",
-          ""minLength"": 1,
-          ""maxLength"": 15
-        },
-        ""depositDate"": {
-          ""description"": ""Payment Deposit Date"",
-          ""type"": ""string"",
-          ""format"": ""date""
-        },
-        ""amount"": {
-          ""description"": ""Payment Amount"",
-          ""type"": ""number""
-        },
-        ""eftDetails"": {
-          ""type"": ""object"",
-          ""properties"": {
-            ""bankNumber"": {
+		""paymentMethod"": {
+		  ""description"": ""Payment Method - Electronic Funds Transfer, Cheque"",
+		  ""type"": ""string"",
+		  ""enum"": [ ""EFT"", ""CHQ"" ]
+		},
+		""paymentType"": {
+		  ""description"": ""Payment Type - Standard, Cancellation, Reversal"",
+		  ""type"": ""string"",
+		  ""enum"": [ ""STD"", ""CAN"", ""REV"" ]
+		},
+		""transactionType"": {
+		  ""description"": ""Transaction Type - Credit (CR) or Debit (DR)"",
+		  ""type"": ""string"",
+          ""enum"": [ ""CR"", ""DR"" ]
+		},
+		""paymentNote"": {
+		  ""description"": ""Payment Note"",
+		  ""type"": ""string"",
+		  ""maxLength"": 20
+		},
+		""systemCode"": {
+		  ""description"": ""Payment Method - Electronic Funds Transfer, Cheque"",
+		  ""type"": ""string"",
+		  ""enum"": [ ""UNIFI"", ""UN"", ""CAN"" ]
+		},
+		""currency"": {
+		  ""description"": ""Currency"",
+		  ""type"": ""string"",
+		  ""enum"": [ ""CAD"", ""USD"" ]
+		},
+		""companyCode"": {
+		  ""description"": ""Company Code"",
+		  ""type"": ""string"",
+		  ""minLength"": 1,
+		  ""maxLength"": 1
+		},
+		""authorizationNumber"": {
+		  ""description"": ""Authorization Number - Unique ID identifying payment (IE. PaymentID)"",
+		  ""type"": ""string"",
+		  ""minLength"": 1,
+		  ""maxLength"": 10
+		},
+		""contractDealerNumber"": {
+		  ""description"": ""Contract Dealer Number"",
+		  ""type"": ""string"",
+		  ""minLength"": 1,
+		  ""maxLength"": 15
+		},
+		""filingDate"": {
+		  ""description"": ""Payment Date"",
+		  ""type"": ""string"",
+		  ""format"": ""date""
+		},
+		""languageCode"": {
+		  ""description"": ""Language"",
+		  ""type"": ""string"",
+		  ""minLength"": 1,
+		  ""maxLength"": 1
+		},
+		""amount"": {
+		  ""description"": ""Payment Amount"",
+		  ""type"": ""number""
+		},
+		""eftDetails"": {
+		  ""type"": ""object"",
+		  ""properties"": {
+			""bankNumber"": {
+			  ""type"": ""string"",
+			  ""minLength"": 4,
+			  ""maxLength"": 4
+			},
+			""branch"": {
+			  ""type"": ""string"",
+			  ""minLength"": 5,
+			  ""maxLength"": 5
+			},
+			""bankAccount"": {
+			  ""type"": ""string"",
+			  ""minLength"": 18,
+			  ""maxLength"": 18
+			},
+			""beneficiaryName"": {
+			  ""type"": ""string"",
+			  ""maxLength"": 30
+			}
+		  }
+		},
+		""chequeDetails"": {
+		  ""type"": ""object"",
+		  ""properties"": {
+			""chequeNumber"": {
+			  ""description"": ""Cheque Number"",
+			  ""type"": ""string"",
+			  ""minLength"": 1,
+			  ""maxLength"": 10
+			},
+		    ""payeeName"": {
+		      ""description"": ""Payee Name - Recipient of Cheque"",
               ""type"": ""string"",
-              ""minLength"": 4,
-              ""maxLength"": 4
-            },
-            ""branch"": {
-              ""type"": ""string"",
-              ""minLength"": 5,
-              ""maxLength"": 5
-            },
-            ""bankAccount"": {
-              ""type"": ""string"",
-              ""minLength"": 18,
-              ""maxLength"": 18
-            },
-            ""beneficiaryName"": {
-              ""type"": ""string"",
-              ""maxLength"": 30
-            }
-          }
-        }
+              ""minLength"": 1,
+              ""maxLength"": 75
+		    },
+			""address"": {
+			  ""description"": ""Cheque Mailing Address"",
+			  ""type"": ""object"",
+			  ""properties"": {
+				""addressLine1"": {
+				  ""type"": ""string"",
+				  ""maxLength"": ""75""
+				},
+				""addressLine2"": {
+				  ""type"": ""string"",
+				  ""maxLength"": ""75""
+				},
+				""addressLine3"": {
+				  ""type"": ""string"",
+				  ""maxLength"": ""75""
+				},
+				""addressLine4"": {
+				  ""type"": ""string"",
+				  ""maxLength"": ""75""
+				},
+				""addressLine5"": {
+				  ""type"": ""string"",
+				  ""maxLength"": ""75""
+				}
+			  }
+		    }
+		  }
+		}
       }
     }
   }
